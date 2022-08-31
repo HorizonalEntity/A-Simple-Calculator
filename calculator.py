f7896c70e9df50eca2e0ae261697e5546eaec61d
HorizonalEntity <112574893+HorizonalEntity@users.noreply.github.com>
2022-08-31 23:02:17 +0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,28 @@
+
+print("Calculator v1.0")
+print( )
+
+
+num1 = input("Insert a Number: ")
+variable = input("Insert a variable(+, -, *, /): ")
+num2 = input("Insert a number: ")
+
+add = float(num1) + float(num2)
+subtract = float(num1) - float(num2)
+multiply = float(num1) * float(num2)
+divide = float(num1) / float(num2)
+
+if variable == "+":
+    print ("your result: add")
+
+elif variable == "-":
+        print(subtract)
+
+elif variable == "*":
+    print(multiply)
+
+elif variable == "/":
+    print(divide)
+
+else:
+  print("error! invalid input")
